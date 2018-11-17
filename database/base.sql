80657fd57fe1f5ee06be2da3616ce9b41c8d1dd6
Yann Julienne <yjulienne@enseirb-matmeca.fr>
2018-11-17 11:34:12 +0100

    Réorganisation du code de la base.

--- database/base.sql
+++ database/base.sql
@@ -12,7 +12,7 @@ drop table FINANCEMENTS cascade constraints;
 
 drop table OBJECTIFS cascade constraints;
 
-drop table POURSUIVRE cascade constraints;
+drop table POURSUITES cascade constraints;
 
 drop table ADHERENTS cascade constraints;
 
@@ -36,16 +36,23 @@ drop table PARTICIPATIONS cascade constraints;
 
 drop table ORGANISATEURS cascade constraints;
 
--- ============================================================
---   Table : ASSOCIATIONS                                            
--- ============================================================
-create table ASSOCIATIONS
-(
-    ID_ASSOCIATION                  NUMBER(3)              not null,
-    NOM_ASSOCIATION                 CHAR(20)               not null,
-    constraint pk_association primary key (ID_ASSOCIATION)
-);
 
+-- ============================================================
+--   Table : LIEUX                                       
+-- ============================================================
+create table LIEUX
+(
+    NOM_LIEU                        CHAR(40)               not null,
+    constraint pk_lieux primary key (NOM_LIEU)
+);
+-- ============================================================
+--   Table : FILIERES                                       
+-- ============================================================
+create table FILIERES
+(
+    NOM_FILIERE                     NUMBER(3)              not null,
+    constraint pk_filieres primary key (NOM_FILIERE)
+);
 -- ============================================================
 --   Table : SOURCE_FINANCEMENTS                                       
 -- ============================================================
@@ -55,22 +62,6 @@ create table SOURCE_FINANCEMENTS
     constraint pk_source_financement primary key (NOM_SOURCE_FINANCEMENT)
 );
 -- ============================================================
---   Table : FINANCEMENTS                                            
--- ============================================================
-create table FINANCEMENTS
-(
-    ID_ASSOCIATION_FINANCEE         NUMBER(3)              not null,
-    NOM_SOURCE_FINANCANT            CHAR(20)               not null,
-    constraint pk_financements primary key (ID_ASSOCIATION_FINANCEE, NOM_SOURCE_FINANCANT)
-);
-alter table FINANCEMENTS
-    add constraint fk1_financements foreign key (ID_ASSOCIATION_FINANCEE)
-       references ASSOCIATIONS (ID_ASSOCIATION);
-
-alter table FINANCEMENTS
-    add constraint fk2_financements foreign key (NOM_SOURCE_FINANCANT)
-       references SOURCE_FINANCEMENTS (NOM_SOURCE_FINANCEMENT);
--- ============================================================
 --   Table : OBJECTIFS                                       
 -- ============================================================
 create table OBJECTIFS
@@ -79,24 +70,55 @@ create table OBJECTIFS
     DESCRIPTION_OBJECTIF            CHAR(255)              not null,
     constraint pk_objectifs primary key (ID_OBJECTIF)
 );
+
+
+
 -- ============================================================
---   Table : POURSUIVRE                                            
+--   Table : ASSOCIATIONS                                            
 -- ============================================================
-create table POURSUIVRE
+create table ASSOCIATIONS
+(
+    ID_ASSOCIATION                  NUMBER(3)              not null,
+    NOM_ASSOCIATION                 CHAR(20)               not null,
+    constraint pk_association primary key (ID_ASSOCIATION)
+);
+-- ============================================================
+--   Table : FINANCEMENTS                                            
+-- ============================================================
+create table FINANCEMENTS
+(
+    ID_ASSOCIATION_FINANCEE         NUMBER(3)              not null,
+    NOM_SOURCE_FINANCANT            CHAR(20)               not null,
+    constraint pk_financements primary key (ID_ASSOCIATION_FINANCEE, NOM_SOURCE_FINANCANT),
+    constraint fk1_financements foreign key (ID_ASSOCIATION_FINANCEE) references ASSOCIATIONS (ID_ASSOCIATION),
+    constraint fk2_financements foreign key (NOM_SOURCE_FINANCANT) references SOURCE_FINANCEMENTS (NOM_SOURCE_FINANCEMENT)
+);
+-- ============================================================
+--   Table : POURSUITES                                            
+-- ============================================================
+create table POURSUITES
 (
     ID_ASSOCIATION_POURSUIVANT      NUMBER(3)              not null,
     ID_OBJECTIF_POURSUIVI           NUMBER(3)              not null,
-    constraint pk_poursuivre primary key (ID_ASSOCIATION_POURSUIVANT, ID_OBJECTIF_POURSUIVI)
+    constraint pk_poursuites primary key (ID_ASSOCIATION_POURSUIVANT, ID_OBJECTIF_POURSUIVI),
+    constraint fk1_poursuites foreign key (ID_ASSOCIATION_POURSUIVANT) references ASSOCIATIONS (ID_ASSOCIATION),
+    constraint fk2_poursuites foreign key (ID_OBJECTIF_POURSUIVI) references OBJECTIFS (ID_OBJECTIF)
+    -- TODO : AJOUTER UNE CONTRAINTE POUR LA CARDINALITÉ MINIMALE (POURSUIVRE->ASSOCATION, POURSUIVRE->OBJECTIF)--
 );
-alter table POURSUIVRE
-    add constraint fk1_poursuivre foreign key (ID_ASSOCIATION_POURSUIVANT)
-       references ASSOCIATIONS (ID_ASSOCIATION);
 
-alter table POURSUIVRE
-    add constraint fk2_poursuivre foreign key (ID_OBJECTIF_POURSUIVI)
-       references OBJECTIFS (ID_OBJECTIF);
--- TODO : AJOUTER UNE CONTRAINTE POUR LA CARDINALITÉ MINIMALE (POURSUIVRE->ASSOCATION, POURSUIVRE->OBJECTIF)--
 
+
+
+-- ============================================================
+--   Table : PERSONNES                                       
+-- ============================================================
+create table PERSONNES
+(
+    ID_PERSONNE                     NUMBER(3)              not null,
+    NOM_PERSONNE                    CHAR(60)               not null,
+    PRENOM_PERSONNE                 CHAR(255)              not null,
+    constraint pk_personnes primary key (ID_PERSONNE)
+);
 -- ============================================================
 --   Table : ADHERENTS                                       
 -- ============================================================
@@ -108,7 +130,10 @@ create table ADHERENTS
     EMAIL_ADHERENT                  CHAR(80)               not null,
     PROMOTION_ADEHRENT              NUMBER(4)              not null,
     FILIERE_ADHERENT                NUMBER(3)              not null,
-    constraint pk_adherant primary key (ID_ADHERENT)
+    constraint pk_adherant primary key (ID_ADHERENT)               ,
+    constraint fk1_adherents foreign key (ID_ADHERENT) references PERSONNES (ID_PERSONNE),
+    constraint fk2_adherents foreign key (FILIERE_ADHERENT) references FILIERES (NOM_FILIERE)
+    -- TODO : AJOUTER UNE CONTRAINTE POUR LA CARDINALITÉ MINIMALE (ADHESIONS->ADHERENTS)--
 );
 -- ============================================================
 --   Table : ADHESIONS                                            
@@ -119,28 +144,12 @@ create table ADHESIONS
     ID_ADHERENT_ADHERANT            NUMBER(3)              not null,
     DATE_ADHESION                   DATE                   not null,
     ROLE_ADHERENT                   CHAR(16)                       ,
-    COTISATION_REGLEE_ADHESION      NUMBER(1)              not null -- Use as boolean
+    COTISATION_REGLEE_ADHESION      NUMBER(1)              not null, -- Use as boolean
+    constraint fk1_adhesions foreign key (ID_ASSOCIATION_ADHEREE) references ASSOCIATIONS (ID_ASSOCIATION),
+    constraint fk2_adhesions foreign key (ID_ADHERENT_ADHERANT) references ADHERENTS (ID_ADHERENT)
 );
-alter table ADHESIONS
-    add constraint fk1_adhesions foreign key (ID_ASSOCIATION_ADHEREE)
-       references ASSOCIATIONS (ID_ASSOCIATION);
 
-alter table ADHESIONS
-    add constraint fk2_adhesions foreign key (ID_ADHERENT_ADHERANT)
-       references ADHERENTS (ID_ADHERENT);
--- TODO : AJOUTER UNE CONTRAINTE POUR LA CARDINALITÉ MINIMALE (ADHESIONS->ADHERENTS)--
 
--- ============================================================
---   Table : FILIERES                                       
--- ============================================================
-create table FILIERES
-(
-    NOM_FILIERE                     NUMBER(3)              not null,
-    constraint pk_filieres primary key (NOM_FILIERE)
-);
-alter table ADHERENTS
-    add constraint fk2_adherents foreign key (FILIERE_ADHERENT)
-       references FILIERES (NOM_FILIERE);
 
 
 -- ============================================================
@@ -152,24 +161,6 @@ create table CONTENUS
     constraint pk_contenus primary key (ID_CONTENU)
 );
 -- ============================================================
---   Table : NEWS                                       
--- ============================================================
-create table NEWS
-(
-    ID_NEWS                      NUMBER(3)              not null,
-    ID_AUTEUR                       NUMBER(3)              not null,
-    TITRE_NEWS                      CHAR(60)               not null,
-    TEXTE_NEWS                      CHAR(255)              not null,
-    DATE_PUBLICATION_NEWS           DATE                   not null,
-    constraint pk_news primary key (ID_NEWS)
-);
-alter table NEWS
-    add constraint fk1_news foreign key (ID_NEWS)
-       references CONTENUS (ID_CONTENU);
-alter table NEWS
-    add constraint fk2_news foreign key (ID_AUTEUR)
-       references ADHERENTS (ID_ADHERENT);
--- ============================================================
 --   Table : COMMENTAIRES                                            
 -- ============================================================
 create table COMMENTAIRES
@@ -179,35 +170,29 @@ create table COMMENTAIRES
     ID_CONTENU_COMMENTE             NUMBER(3)              not null,
     DATE_PUBLICATION_COMENTAIRE     DATE                   not null,
     TEXTE_COMMENTAIRE               CHAR(255)              not null,
-    constraint pk_commentaire primary key (ID_COMMENTAIRE) 
+    constraint pk_commentaire primary key (ID_COMMENTAIRE)         ,
+    constraint fk1_commentaires foreign key (ID_ADHERENT_COMMENTANT) references ADHERENTS (ID_ADHERENT),
+    constraint fk2_commentaires foreign key (ID_CONTENU_COMMENTE) references CONTENUS (ID_CONTENU)
 );
-alter table COMMENTAIRES
-    add constraint fk1_commentaires foreign key (ID_ADHERENT_COMMENTANT)
-       references ADHERENTS (ID_ADHERENT);
-alter table COMMENTAIRES
-    add constraint fk2_commentaires foreign key (ID_CONTENU_COMMENTE)
-       references CONTENUS (ID_CONTENU);
+
+
 -- ============================================================
---   Table : PERSONNES                                       
+--   Table : NEWS                                       
 -- ============================================================
-create table PERSONNES
+create table NEWS
 (
-    ID_PERSONNE                     NUMBER(3)              not null,
-    NOM_PERSONNE                    CHAR(60)               not null,
-    PRENOM_PERSONNE                 CHAR(255)              not null,
-    constraint pk_personnes primary key (ID_PERSONNE)
+    ID_NEWS                         NUMBER(3)              not null,
+    ID_AUTEUR                       NUMBER(3)              not null,
+    TITRE_NEWS                      CHAR(60)               not null,
+    TEXTE_NEWS                      CHAR(255)              not null,
+    DATE_PUBLICATION_NEWS           DATE                   not null,
+    -- TODO : Ajouter une contrainte pour empêcher un évènement d'être un commentaire
+    constraint pk_news primary key (ID_NEWS)                       ,
+    constraint fk1_news foreign key (ID_NEWS) references CONTENUS (ID_CONTENU),
+    constraint fk2_news foreign key (ID_AUTEUR) references ADHERENTS (ID_ADHERENT)
 );
-alter table ADHERENTS
-    add constraint fk1_adherents foreign key (ID_ADHERENT)
-       references PERSONNES (ID_PERSONNE);
--- ============================================================
---   Table : LIEUX                                       
--- ============================================================
-create table LIEUX
-(
-    NOM_LIEU                        CHAR(40)               not null,
-    constraint pk_lieux primary key (NOM_LIEU)
-);
+
+
 -- ============================================================
 --   Table : EVENEMENTS                                       
 -- ============================================================
@@ -225,16 +210,11 @@ create table EVENEMENTS
     DATE_FIN_RECURRENCE_EVENEMENT   DATE                           ,
     FREQUENCE                       CHAR(15)                       ,
     -- TODO : Ajouter contrainte énumération + contrainte DATE_FIN_RECURENCE => FREQUENCE
-    constraint pk_evenements primary key (ID_EVENEMENT)
+    -- TODO : Ajouter une contrainte pour empêcher une news d'être un évènement
+    constraint pk_evenements primary key (ID_EVENEMENT)            ,
+    constraint fk1_evenements foreign key (ID_EVENEMENT) references CONTENUS (ID_CONTENU),
+    constraint fk2_evenements foreign key (LIEU_EVENEMENT) references LIEUX (NOM_LIEU)
 );
-alter table EVENEMENTS
-    add constraint fk1_evenements foreign key (ID_EVENEMENT)
-       references PERSONNES (ID_PERSONNE);
-alter table EVENEMENTS
-    add constraint fk2_evenements foreign key (LIEU_EVENEMENT)
-       references LIEUX (NOM_LIEU);
--- TODO : Ajouter une contrainte pour empêcher une news d'être un évènement
-
 -- ============================================================
 --   Table : PARTICIPATIONS                                       
 -- ============================================================
@@ -243,14 +223,10 @@ create table PARTICIPATIONS
     ID_PERSONNE_PARTICIPANT         NUMBER(3)              not null,
     ID_EVENEMENT_PARTICIPE          NUMBER(3)               not null,
     NOTE                            CHAR(1)                        ,
-    constraint pk_participations primary key (ID_PERSONNE_PARTICIPANT, ID_EVENEMENT_PARTICIPE)
+    constraint pk_participations primary key (ID_PERSONNE_PARTICIPANT, ID_EVENEMENT_PARTICIPE),
+    constraint fk1_participations foreign key (ID_PERSONNE_PARTICIPANT) references PERSONNES (ID_PERSONNE),
+    constraint fk2_participations foreign key (ID_EVENEMENT_PARTICIPE) references EVENEMENTS (ID_EVENEMENT)
 );
-alter table PARTICIPATIONS
-    add constraint fk1_participations foreign key (ID_PERSONNE_PARTICIPANT)
-       references PERSONNES (ID_PERSONNE);
-alter table PARTICIPATIONS
-    add constraint fk2_participations foreign key (ID_EVENEMENT_PARTICIPE)
-       references EVENEMENTS (ID_EVENEMENT);
 -- ============================================================
 --   Table : ORGANISATEURS                                       
 -- ============================================================
@@ -258,11 +234,8 @@ create table ORGANISATEURS
 (
     ID_ADHERENT_ORGANISATEUR        NUMBER(3)              not null,
     ID_EVENEMENT_ORGANISE           NUMBER(3)               not null,
-    constraint pk_organisateurs primary key (ID_ADHERENT_ORGANISATEUR, ID_EVENEMENT_ORGANISE)
+    -- TODO : AJOUTER UNE CONTRAINTE POUR LA CARDINALITÉ MINIMALE (ORAGNISATEURS->EVENEMENTS)--
+    constraint pk_organisateurs primary key (ID_ADHERENT_ORGANISATEUR, ID_EVENEMENT_ORGANISE),
+    constraint fk1_organisateurs foreign key (ID_ADHERENT_ORGANISATEUR) references ADHERENTS (ID_ADHERENT),
+    constraint fk2_organisateurs foreign key (ID_EVENEMENT_ORGANISE) references EVENEMENTS (ID_EVENEMENT)
 );
-alter table ORGANISATEURS
-    add constraint fk1_organisateurs foreign key (ID_ADHERENT_ORGANISATEUR)
-       references ADHERENTS (ID_ADHERENT);
-alter table ORGANISATEURS
-    add constraint fk2_organisateurs foreign key (ID_EVENEMENT_ORGANISE)
-       references EVENEMENTS (ID_EVENEMENT);
