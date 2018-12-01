16cc754175aaabb3410627076e183d3cf6369835
Yann Julienne <yann.j@live.fr>
2018-12-01 10:45:09 +0100

    Modification de la reque précédente pour apporter un classement des évènements en fonction de la moyenne de leurs notes.

--- database/views/create.sql
+++ database/views/create.sql
@@ -1,3 +1,3 @@
 @views/adherents_non_cotisant/create
 @views/nb_commentaires_par_news/create
-@views/notes_moyennes_evenement/create
+@views/classement_evenements/create
--- database/views/drop.sql
+++ database/views/drop.sql
@@ -1,3 +1,3 @@
 @views/adherents_non_cotisant/drop
 @views/nb_commentaires_par_news/drop
-@views/notes_moyennes_evenement/drop
+@views/classement_evenements/drop
