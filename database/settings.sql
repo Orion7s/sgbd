cc7bc19553d9115d93041680228b8468911f55b8
Yann Julienne <yann.j@live.fr>
2018-12-03 11:58:35 +0100

    Modifications mineures de mise en forme.

--- database/settings.sql
+++ database/settings.sql
@@ -1,6 +1,9 @@
 begin
+    -- MOIS-JOUR spécifiant la date de prise d'effet d'une adhésion.
     dbsettings.adhesion_date_fin := '08-31';
+    -- MOIS-JOUR spécifiant le date de fin d'effet d'une adhésion.
     dbsettings.adhesion_date_debut := '09-01';
+    -- Libellé de la ligne des cotisations dans les requêtes statistisques concernant les financements.
     dbsettings.nom_cotisation := 'COTISATIONS';
 end;
 /
