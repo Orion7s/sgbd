3cd7f41a932892f76cd3b1d9b03912d384020411
Yann Julienne <yann.j@live.fr>
2018-12-04 11:21:53 +0100

    AJout à la base de données de la contrainte forcant un adhérents à faire partir de l'association d'un évènement qu'il organise.

--- database/tables/financements.sql
+++ database/tables/financements.sql
@@ -7,7 +7,7 @@ create table FINANCEMENTS
     NOM_SOURCE_FINANCANT            CHAR(20)               not null,
     MONTANT                         NUMBER(6)              not null,
     DATE_FINANCEMENT                DATE                   not null,
-    constraint pk_financements primary key (ID_ASSOCIATION_FINANCEE, NOM_SOURCE_FINANCANT),
+    constraint pk_financements primary key (ID_ASSOCIATION_FINANCEE, NOM_SOURCE_FINANCANT, DATE_FINANCEMENT),
     constraint fk1_financements foreign key (ID_ASSOCIATION_FINANCEE) 
       references ASSOCIATIONS (ID_ASSOCIATION) on delete cascade,
     constraint fk2_financements foreign key (NOM_SOURCE_FINANCANT) 
--- database/tables/organisateurs.sql
+++ database/tables/organisateurs.sql
@@ -12,8 +12,27 @@ create table ORGANISATEURS
     constraint fk2_organisateurs foreign key (ID_EVENEMENT_ORGANISE) 
       references EVENEMENTS (ID_EVENEMENT) on delete cascade
 );
+CREATE TRIGGER organisateurs_roc
+  BEFORE INSERT ON ORGANISATEURS  
+  FOR EACH ROW
+DECLARE
+  asso_ev number;
+  adh_count number;
+BEGIN
+  select ID_ASSOCIATION_HOTE INTO asso_ev FROM EVENEMENTS WHERE ID_EVENEMENT = :new.ID_EVENEMENT_ORGANISE;
+  select COUNT(ID_ASSOCIATION_ADHEREE) INTO adh_count 
+  FROM ADHESIONS WHERE ID_ADHERENT_ADHERANT = :new.ID_ADHERENT_ORGANISATEUR 
+                  and ID_ASSOCIATION_ADHEREE = asso_ev;
+  IF( adh_count = 0 )
+  THEN
+    RAISE_APPLICATION_ERROR( -20001, 'Evenement et adherent doivent être organisé/adérer à la même association.' );
+  END IF;
+END;
+/
+show errors trigger organisateurs_roc;
 -- Déclaration des objets créés
 begin
     register_object('table', 'ORGANISATEURS');
+    register_object('trigger', 'organisateurs_roc');
 end;
 /
